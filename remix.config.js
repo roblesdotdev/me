1505647b621b3c5394b55f5a316d43e354e85d6c
roblesdotdev <httpsnip@gmail.com>
2024-03-23 19:00:59 -0300

    serve remix 404 with gh pages

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,7 @@
 import { flatRoutes } from 'remix-flat-routes'
+import { copyFileSync } from 'node:fs'
+import { join } from 'node:path'
+
 /** @type {import('@remix-run/dev/dist/vite/plugin').VitePluginConfig} */
 export default {
   basename: '/me/',
@@ -21,5 +24,19 @@ export default {
         '**/*.client.*',
       ],
     })
+  },
+  buildEnd(args) {
+    if (!args.viteConfig.isProduction) return
+
+    // When deploying to GitHub Pages, if you navigate from / to another
+    // route and refresh the tab, it will show the default GH Pages 404
+    // page. This happens because GH Pages is not configured to send all
+    // traffic to the index.html where we can load our client-side JS.
+    // To fix this, we can create a 404.html file that contains the same
+    // content as index.html. This way, when the user refreshes the page,
+    // GH Pages will serve our 404.html and everything will work as
+    // expected.
+    const buildPath = args.viteConfig.build.outDir
+    copyFileSync(join(buildPath, 'index.html'), join(buildPath, '404.html'))
   },
 }
