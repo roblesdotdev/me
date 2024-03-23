c3b984ab83a47ffa8833803d30f57041186e3b7e
roblesdotdev <httpsnip@gmail.com>
2024-03-23 16:52:38 -0300

    remix/vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import remixConfig from "./remix.config";
+
+const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ssr: false,
-    }),
-    tsconfigPaths(),
-  ],
+  build: { minify: isProd ? "terser" : false, sourcemap: true, ssr: false },
+  plugins: [remix(remixConfig), tsconfigPaths()],
 });
