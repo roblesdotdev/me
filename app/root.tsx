324fa63add70e580535112da43c906205f8e79b5
roblesdotdev <httpsnip@gmail.com>
2024-03-23 16:19:07 -0300

    setup tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,6 +6,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import globalStylesHref from "~/styles/global.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesHref },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
